5ea8f5c130e012f6ee66621e48186aeeaaab9388
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-21 00:15:56 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function sum(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+
+console.log('Сума:', sum(3,5));
+console.log('Віднімання:', subtraction(4,8));
+console.log('Множення:', multiplication(7, 10));
