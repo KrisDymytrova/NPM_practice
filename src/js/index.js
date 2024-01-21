010f00f9e7675b90f8b9e3eecbdc170136ee1a6a
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-01-21 11:28:23 +0200

    "Install Prettier"

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtraction(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiplication(a, b) {
-    return a * b;
+  return a * b;
 }
 
-
-console.log('Сума:', sum(3,5));
-console.log('Віднімання:', subtraction(4,8));
-console.log('Множення:', multiplication(7, 10));
+console.log("Сума:", sum(3, 5));
+console.log("Віднімання:", subtraction(4, 8));
+console.log("Множення:", multiplication(7, 10));
